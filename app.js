af360cd83116b871af381a57503728c4cbd400b7
B-A-Sh <baramos19@gmail.com>
2025-01-12 18:37:33 +0200

    begin merging

--- app.js
+++ app.js
@@ -18,15 +18,43 @@ config();
 const PORT = process.env.PORT || 8080;
 
 //who can connect to me and if i can recieve data from the browser
-app.use(cors({origin: 'http://localhost:5173',credentials: true}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api/v1/users',useRouter);
 
-socketMW()
+// socketMW()
 
 //----------------------------------------------------------------
+
+io.on("connection",(socket) => {
+    console.log(`user connected in socket number ${socket.id}`);
+    socket.on("sendMessage",(message,room)=> {
+        const messageObject = {
+            userObject: message.currentUserObject,
+            messageId: Date.now(),
+            timeSent: new Date(Date.now()).toLocaleTimeString('en-US',{
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false}),
+            content :message.userMsg
+        }
+        if(room){
+            socket.to(room).emit("receivePrivateMessage",messageObject)
+        }else
+            io.emit("receiveMessage",messageObject)
+            // socket.broadcast.emit("receiveMessage",messageObject)
+    })
+    socket.on('join-room',(room)=>{
+        socket.join(room)
+        console.log(`socket-- ${socket.id} added to room name -- ${room}`);
+                                   
+    })
+    // socket.on('join-game',(gameName,chatId,currentUserObject)=>{
+        // פה אדל שמה את הקוד שלה        
+
+    // }
 
 
 //----------------------------------------------------------------
